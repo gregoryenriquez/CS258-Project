eb0c982388620b842b7d14e9d24f57c57cb27dc1
gregoryenriquez <gregoryenriquez@gmail.com>
2016-11-15 00:16:17 -0800

    working cs258test.py

--- cs258test.py
+++ cs258test.py
@@ -1,14 +1,19 @@
-"""Custom topology example
+"""
+Script to start mininet with remote controller and a full mesh 4 switch network
 
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
+
 from mininet.topo import Topo
+from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+setLogLevel('info')
+
+c0 = RemoteController('c0', ip="192.168.56.101", port=6633)
+
 
 class MyTopo( Topo ):
     "Simple topology example."
@@ -20,7 +25,7 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        H1 = self.addHost( 'h1',ip='192.168.56.102/24' )
+        H1 = self.addHost( 'h1' )
         H2 = self.addHost( 'h2' )
 	H3 = self.addHost('h3')
 	H4 = self.addHost('h4')
@@ -47,4 +52,15 @@ class MyTopo( Topo ):
 	self.addLink( S3, S4 )
 
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+topo = MyTopo()
+
+net = Mininet(topo=topo, controller=c0)
+try:
+#	net.addController(c0)
+	net.start()
+	CLI(net)
+
+except Exception,e:
+	print(str(e)) 
+	net.stop()
+net.stop()
