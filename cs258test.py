7afba27ed048dd841ab42708a7e747a3ee9f0731
gregoryenriquez <gregoryenriquez@gmail.com>
2016-11-27 20:35:22 -0800

    hard code ipv4 addresses

--- cs258test.py
+++ cs258test.py
@@ -9,6 +9,7 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+from mininet.link import Intf
 
 setLogLevel('info')
 
@@ -18,6 +19,7 @@ c0 = RemoteController('c0', ip="192.168.56.101", port=6633)
 class MyTopo( Topo ):
     "Simple topology example."
 
+
     def __init__( self ):
         "Create custom topo."
 
@@ -25,16 +27,17 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        H1 = self.addHost( 'h1' )
-        H2 = self.addHost( 'h2' )
-	H3 = self.addHost('h3')
-	H4 = self.addHost('h4')
+        H1 = self.addHost( 'h1', ip="10.0.1.1/24" )
+        H2 = self.addHost( 'h2', ip="10.0.1.2/24" )
+	H3 = self.addHost( 'h3'  ip="10.0.1.3/24" )
+	H4 = self.addHost( 'h4', ip="10.0.1.4/24" )
         
 
 	S1 = self.addSwitch( 's1' )
         S2 = self.addSwitch( 's2' )
 	S3 = self.addSwitch( 's3' )
 	S4 = self.addSwitch( 's4' )
+	S5 = self.addSwitch( 's5' )
 
         # Add links
         self.addLink( H1, S1 )
@@ -42,23 +45,29 @@ class MyTopo( Topo ):
 	self.addLink( H3, S3 )
 	self.addLink( H4, S4 )
 
+	self.addLink(S1, S5)
+	self.addLink(S2, S5)
+	self.addLink(S3, S5)
+	self.addLink(S4, S5)
+
         self.addLink( S1, S2 )
-	self.addLink( S1, S3 )
+	#self.addLink( S1, S3 )
 	self.addLink( S1, S4 )
 
 	self.addLink( S2, S3 )
-	self.addLink( S2, S4 )
+	#self.addLink( S2, S4 )
 
 	self.addLink( S3, S4 )
+
 
 
 topo = MyTopo()
 
 net = Mininet(topo=topo, controller=c0)
 try:
-#	net.addController(c0)
 	net.start()
 	CLI(net)
+
 
 except Exception,e:
 	print(str(e)) 
